9b969f99ee9c72bf2cb1358bbfcbef68cd0b9a93
John Roche <jroche@redhat.com>
2018-03-27 20:34:21 +0100

    Integrate with client-service-associations

--- app/scripts/directives/mobileClientConfig.js
+++ app/scripts/directives/mobileClientConfig.js
@@ -13,50 +13,72 @@ angular.module('openshiftConsole').component('mobileClientConfig', {
       MobileClientConfigCtrl]
   });
 
+var filterExcludedServices = function(serviceInstances, mobileClient) {
+  return _.filter(serviceInstances, function(service, serviceName){
+    return _.indexOf(_.get(mobileClient, 'spec.excludedServices'), serviceName) === -1;
+  });
+};
+
+var getServiceConfig = function(configmaps, services) {
+  return _(configmaps.by('metadata.name'))
+  .filter(function(configmap) {
+    return  _.findIndex(services, {metadata: {labels: {serviceName: _.get(configmap, 'metadata.name')}}}) !== -1;
+  })
+  .map(function(configmap) {
+    return {
+      id: _.get(configmap, 'metadata.name'),
+      name: _.get(configmap, 'metadata.name'),
+      type: configmap.data.type,
+      url: configmap.data.uri,
+      config: configmap.data
+    };
+  }).value();
+};
+
+var getClientConfig = function(mobileClient, serviceConfig, clusterInfo) {
+  return JSON.stringify({
+    version: 1,
+    clusterName: clusterInfo.openshift.hostPort,
+    namespace: _.get(mobileClient, 'metadata.namespace'),
+    clientId: _.get(mobileClient, 'metadata.name'),
+    services: serviceConfig
+  }, null, '  ');
+};
+
 function MobileClientConfigCtrl(APIService, DataService, API_CFG) {
   var ctrl = this;
   var watches = [];
   ctrl.$onInit = function(){
-    var context = {namespace: _.get(ctrl, "mobileClient.metadata.namespace")};
+    var context = {namespace: _.get(ctrl, 'mobileClient.metadata.namespace')};
     //keep list of active services upto date
     DataService.watch(
       APIService.getPreferredVersion('serviceinstances'), 
       context, 
       function (serviceinstances){
-        ctrl.services = _.filter(serviceinstances.by("metadata.name"), function(service, serviceName){
-          return _.indexOf(ctrl.mobileClient.spec.excludedServices, serviceName) === -1;
-        });
-        DataService.list(APIService.getPreferredVersion('configmaps'), context, updateConfig, {errorNotification: false});
+        ctrl.services = serviceinstances.by('metadata.name');
+        DataService.list(APIService.getPreferredVersion('configmaps'), context, updateClientConfig, {errorNotification: false});
       }, 
       { errorNotification: false }
     );
-    watches.push(DataService.watch(APIService.getPreferredVersion('configmaps'), context, updateConfig, {errorNotification: false}));
+    watches.push(DataService.watch(APIService.getPreferredVersion('configmaps'), context, updateClientConfig, {errorNotification: false}));
 
     // update the config string by pulling out configmaps that match ctrl.services
-    function updateConfig(configmaps){
-      var configs =_(configmaps.by("metadata.name"))
-      .filter(function(configmap) {
-        return  _.findIndex(ctrl.services, {metadata: {labels: {serviceName: _.get(configmap, "metadata.name")}}}) !== -1;
-      })
-      .map(function(configmap) {
-        return {
-          id: _.get(configmap, "metadata.name"),
-          name: _.get(configmap, "metadata.name"),
-          type: configmap.data.type,
-          url: configmap.data.uri,
-          config: configmap.data
-        };
-      }).value();
-      ctrl.prettyConfig = JSON.stringify({
-        version: 1,
-        clusterName: API_CFG.openshift.hostPort,
-        namespace: _.get(ctrl, "mobileClient.metadata.namespace"),
-        clientId: _.get(ctrl, "mobileClient.metadata.name"),
-        services: configs
-      }, null, "  ");
+    function updateClientConfig(configmaps){
+      ctrl.configmaps = configmaps;
+      var services = filterExcludedServices(ctrl.services, ctrl.mobileClient);
+      ctrl.serviceConfig = getServiceConfig(configmaps, services);
+      ctrl.prettyConfig = getClientConfig(ctrl.mobileClient, ctrl.serviceConfig, API_CFG);
     }
   };
   
+  ctrl.$onChanges = function(changes) {
+    if (changes.mobileClient && ctrl.configmaps && ctrl.services) {
+      var services = filterExcludedServices(ctrl.services, ctrl.mobileClient);
+      ctrl.serviceConfig = getServiceConfig(ctrl.configmaps, services);
+      ctrl.prettyConfig = getClientConfig(ctrl.mobileClient, ctrl.serviceConfig, API_CFG);
+    }
+  };
+
   ctrl.$onDestroy = function() {
     DataService.unwatchAll(watches);
   };
